561cb4d03582b215d0fdc49abea0fb811e617021
Simran Sandhu <simran7s@my.yorku.ca>
2021-09-26 21:43:00 -0400

    setup for heroku

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 // const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose")
+require("dotenv").config()
+
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -17,7 +19,7 @@ const __HOME_ROUTE = "Today"
 // Removed these arrays and we will now use Mongo to store them instead
 // const items = ["Buy Food", "Cook Food", "Eat Food"];
 // const workItems = [];
-mongoose.connect('mongodb+srv://admin-simran:Password69!@cluster0.0ntyc.mongodb.net/todoListDB');
+mongoose.connect(process.env.MONGO_URL);
 const todosSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -170,6 +172,11 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+// If no port setup on heroku, then start on local host 3000
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
